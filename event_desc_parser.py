7b9d9b68f7d84e5e5b0952354d71e48564e8f42f
Dmitry Gordeev <nillsondg@gmail.com>
2017-12-03 12:57:37 +0300

    fixed flacon parsing

--- event_desc_parser.py
+++ event_desc_parser.py
@@ -622,8 +622,8 @@ def parse_desc_from_flacon(url):
 
     header_info_block = g.doc.select('//section[@class="preview-section_wrap"]').node()
 
-    date_raw = header_info_block.xpath('.//div[@class="date"]')[0].text.strip().lower()
-    time_raw = header_info_block.xpath('.//div[@class="date"]')[0].xpath('.//br')[0].tail.strip()
+    date_raw = header_info_block.xpath('.//div[@class="date"] | .//p[@class="date"]')[0].text.strip().lower()
+    time_raw = header_info_block.xpath('.//div[@class="date"] | .//p[@class="date"]')[0].xpath('.//br')[0].tail.strip()
 
     # 23 ДЕКАБРЯ 2017
     # 27 НОЯБРЯ—3 ДЕКАБРЯ 2017
@@ -725,6 +725,3 @@ def parse_desc_from_flacon(url):
            "image_horizontal": img,
            "filenames": {'horizontal': filename}}
     return res
-
-
-parse_desc_from_flacon("http://flacon.ru/afisha/event/vystavka-russian-sorrow/")
