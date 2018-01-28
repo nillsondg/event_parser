6923bef214e3f871440937ad408797950de71782
Dmitry Gordeev <nillsondg@gmail.com>
2018-01-28 11:52:45 +0300

    fixed a little

--- mincult/min_cult_utils.py
+++ mincult/min_cult_utils.py
@@ -2,17 +2,17 @@ def prepare_location(addr_json):
     place = addr_json
     region = place["region"]["name"]
     try:
-        city = place["city"]["type"] + place["city"]["name"]
+        city = place["city"]["type"] + " " + place["city"]["name"]
     except KeyError:
         city = place["city"]["name"]
 
     try:
-        street = place["street"]["type"] + place["street"]["name"]
+        street = place["street"]["type"] + " " + place["street"]["name"]
     except KeyError:
         street = place["street"]["name"]
 
     try:
-        house = place["house"]["type"] + place["house"]["name"]
+        house = place["house"]["type"] + " " + place["house"]["name"]
     except KeyError:
         try:
             house = place["house"]["name"]
