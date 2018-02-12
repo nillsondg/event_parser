b2adc2ed2815cccd7f76811dd3118425e63aaff3
Dmitry Gordeev <nillsondg@gmail.com>
2018-02-12 21:36:39 +0300

    fixed a little logo generator

--- logo_generator.py
+++ logo_generator.py
@@ -9,18 +9,20 @@ import sys
 def get_string(title):
     r = re.findall('([А-ЯA-Z])', title)
     try:
-        return r[0] + r[1]
+        return r[0] + r[1] + r[2]
     except KeyError:
-        return title[:1].capitalize() + title[1:2].capitalize()
+        try:
+            return r[0] + r[1]
+        except KeyError:
+            return title[:2].capitalize()
 
 
 def generate_logo(title):
-    from PIL import Image
     img_w, img_h = (500, 500)
     img = Image.new('RGB', (img_w, img_h), color=(get_color_int(), get_color_int(), get_color_int()))
     d = ImageDraw.Draw(img)
     fnt = ImageFont.truetype(get_font_path(), 208)
-    w, h = d.textsize(translit(get_string(title).capitalize(), 'ru', reversed=True), font=fnt)
+    w, h = d.textsize(translit(get_string(title), 'ru', reversed=True).capitalize(), font=fnt)
     d.text(((img_w - w) / 2, (img_h - h) / 2), get_string(title), font=fnt, fill=(255, 255, 255))
     img_raw = BytesIO()
     img.save(img_raw, format='PNG')
